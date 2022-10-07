c000101309e7d22a3660baaf6756471682b99e5c
VladislavSoltanov <vsoltanovv@gmail.com>
2022-10-07 11:18:39 +0200

    Добавлен контроллер для представления

---
+++ Controllers/ViewController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SendMessageEmail.Controllers
+{
+    public class ViewController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
