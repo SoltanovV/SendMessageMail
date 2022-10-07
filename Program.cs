5ec6c814f80c329c08570021a7ee82789ba7da4c
VladislavSoltanov <vsoltanovv@gmail.com>
2022-10-07 11:18:05 +0200

    Добавление настроек для сериализации

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +9,28 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Настройка сериализации 
+builder.Services.AddMvc().AddJsonOptions(o => {
+    o.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    o.JsonSerializerOptions.MaxDepth = 0;
+});
+
+
+builder.Services.AddCors(opions =>
+{
+    opions.AddPolicy(name: "CorsPolicy", policy =>
+    {
+        policy.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod();
+    });
+});
+
+string connection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(connection));
+
+
+builder.Services.AddTransient<IMessageServices, MessageServices>();
+builder.Services.AddTransient<LoggerDbServices>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,6 +39,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("CorsPolicy");
 
 app.UseHttpsRedirection();
 
