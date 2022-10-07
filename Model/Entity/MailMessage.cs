3f3fc820391bb5385c55a2d548407ee395c4a75a
VladislavSoltanov <vsoltanovv@gmail.com>
2022-10-07 11:11:47 +0200

    Добавлены сущности для бд

---
+++ Model/Entity/MailMessage.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SendMessageEmail.Model.Entity;
+
+/// <summary>
+/// Mail и текст сообщения
+/// </summary>
+public class MailMessage
+{
+    /// <summary>
+    /// Id сообщения
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Получатель сообщения
+    /// </summary>
+    public string Recepient { get; set; }
+
+    /// <summary>
+    /// Заголовок сообщения
+    /// </summary>
+    public string Subject { get; set; }
+
+    /// <summary>
+    /// Сообщение
+    /// </summary>
+    public string Messages { get; set; }
+
+    public DateTime SenddateTime { get;set; } = DateTime.Now;
+
+    /// <summary>
+    /// Id статуса сообщения
+    /// </summary>
+    [JsonIgnore]
+    public int MessageStatusId { get; set; }
+
+    /// <summary>
+    /// Навигационное свойство
+    /// </summary>
+    public MessageStatus MessageStatus { get; set; }
+}
---
+++ Model/Entity/MessageStatus.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SendMessageEmail.Model.Entity
+{
+    /// <summary>
+    /// Статус сообщения
+    /// </summary>
+    public class MessageStatus
+    {
+        /// <summary>
+        /// Id статуса
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Результата отправки
+        /// </summary>
+        public string Result { get; set; }
+
+        /// <summary>
+        /// Логинрование  
+        /// </summary>
+        public string? Log { get; set; }
+
+        /// <summary>
+        /// Навигационное свойсво
+        /// </summary>
+        [JsonIgnore]
+        public IEnumerable<MailMessage> MailMessage { get; set; }
+    }
+}
