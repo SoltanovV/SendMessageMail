68ca137be3beeda66e5073716358d102f858cfcf
VladislavSoltanov <vsoltanovv@gmail.com>
2022-10-07 11:16:01 +0200

    Модель для маппинга данных в MailMessage

---
+++ ViewModel/Message.cs
@@ -0,0 +1,23 @@
+﻿namespace SendMessageEmail.ViewModel
+{
+    /// <summary>
+    /// Предстваление модели для MailMessage
+    /// </summary>
+    public class Message
+    {
+        /// <summary>
+        /// Email получателя
+        /// </summary>
+        public string Recepient { get; set; }
+
+        /// <summary>
+        /// Тема письма
+        /// </summary>
+        public string Subject { get; set; }
+
+        /// <summary>
+        /// Текс письма
+        /// </summary>
+        public string Messages { get; set; }
+    }
+}
