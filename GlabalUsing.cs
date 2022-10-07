e8b52a74ea8126c944a6a5b4f3037450c66ffa2f
VladislavSoltanov <vsoltanovv@gmail.com>
2022-10-07 11:16:47 +0200

    Файл для глобального подключения библиотек

--- GlabalUsing.cs
+++ GlabalUsing.cs
@@ -1,6 +1,13 @@
-﻿namespace SendMessageEmail
-{
-    public class GlabalUsing
-    {
-    }
-}
+﻿global using SendMessageEmail.Services.Interfaice;
+global using SendMessageEmail.Services;
+
+global using SendMessageEmail.ViewModel;
+
+global using AutoMapper;
+
+global using Microsoft.EntityFrameworkCore;
+global using SendMessageEmail.Model.Entity;
+global using SendMessageEmail.Model;
+
+global using MimeKit;
+global using MailKit.Net.Smtp;
